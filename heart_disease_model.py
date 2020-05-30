40d673f4f2e51913d8254c4b3106972dc79a9919
Alberto Casado <albertocasadopeguero@gmail.com>
2020-05-30 19:18:40 -0400

    Update heart_disease_model.py
    More plotting to get a sense how the features are correlated to the target value

--- heart_disease_model.py
+++ heart_disease_model.py
@@ -28,10 +28,34 @@ women_age_positive = dataset[(dataset['sex'] == 0) & (dataset['target'] == 1)]['
 sns.distplot(women_age_positive,kde = False)
 
 #Distribution of the ones with no heart desease
-#near the 60's is the age when men's suffer it the most
+#We have almost the same kind of age distribution for both sex like the positive ones
 men_age_negative = dataset[(dataset['sex'] == 1) & (dataset['target'] == 0)]['age']
 sns.distplot(men_age_negative,kde = False)
 
-#And for women as well, we can say
 women_age_negative = dataset[(dataset['sex'] == 1) & (dataset['target'] == 0)]['age']
 sns.distplot(women_age_negative,kde = False,bins = 20)
+
+#Plotting the cholesterol and the heart rate, it seems that high cholesterol and heart rate
+#Is almost the same for both people with heart desease and not.
+sns.scatterplot('chol','thalach',hue = 'target',data = dataset)
+
+#Plotting the resting blood pressure and the heart rate
+sns.scatterplot('trestbps','thalach',hue = 'target',data = dataset)
+
+#Trying different plot kinds to try to make more sense of the data
+#It looks the that the heart rate is the most correlated feature to the target
+sns.catplot('target','chol',kind = 'swarm',data = dataset)
+sns.catplot('target','thalach',kind = 'swarm',data = dataset)
+sns.catplot('target','trestbps',kind = 'swarm',data = dataset)
+
+#Plotting the other features with respect to the target
+sns.countplot('cp',hue = 'target',data = dataset)
+sns.countplot('fbs',hue = 'target',data = dataset)
+sns.countplot('restecg',hue = 'target',data = dataset)
+sns.countplot('exang',hue = 'target',data = dataset)
+sns.countplot('slope',hue = 'target',data = dataset)
+sns.countplot('ca',hue = 'target',data = dataset)
+sns.countplot('thal',hue = 'target',data = dataset)
+
+#Checking the correlation of the features, a few features are corralated but not too high
+sns.heatmap(dataset.corr(),annot = True)
